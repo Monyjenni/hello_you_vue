0521b26e2fb173610d80e4341b011bf4a2249922
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-21 00:15:58 +0700

    adding_data_property_without_using_thisKeyword

--- method/src/App.vue
+++ method/src/App.vue
@@ -4,7 +4,7 @@
   <h3>{{ 3+6+8}}</h3>
   <!-- inside curly braces__can call the method  -->
   <h2>Add method- {{ add(10,4,5) }}</h2>
-  <h2>Multiply method -{{ multiply(10) }}</h2>
+  <h2>Multiply method -{{ multiply(baseValue) }}</h2>
 </div>
 </template>
 
@@ -14,8 +14,11 @@
 export default {
   name: 'App',
   data(){
+    //data properties
     return{
-      baseMultiplier : 5
+      baseMultiplier : 5,
+      //if i pass data properties like (baseValue) no need to use this keyword, not use num
+      baseValue:2
     }
   },
   methods:{
@@ -25,7 +28,7 @@ export default {
       return a + b + c
     },
     //if we use arrow then this dont point to the baseMultiplier
-    multiply: (num) => {
+    multiply (num) {
       // baseMultiplier is from return data
       //if no return means nth
       return num * this.baseMultiplier
