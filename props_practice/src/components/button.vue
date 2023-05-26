65882ef699691555ddadec0b9290e6036d77c27d
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-26 15:52:21 +0700

    dynamic_props_passing_data

---
+++ props_practice/src/components/button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+<button @click="generateName" class="btn btn-primary"> Generate UwU!!! </button>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonVue',
+    props :{
+      newName: String
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
