8ad1eaf6a630dc5fb43013248b3c414cc26d1f7c
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-26 14:40:23 +0700

    props_practicing

---
+++ props_practice/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Props Practice</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+
+<style scoped>
+
+</style>
