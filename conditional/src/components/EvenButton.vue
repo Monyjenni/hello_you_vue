42ea92d571f206458d637f877c73924771f0b4be
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-06-05 15:46:06 +0700

    adding_sayName_method

---
+++ conditional/src/components/EvenButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-btn v-on="$listeners" v-bind="$attrs" :color="color">Click Me</v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'eventButton',
+    props : {
+      color : {
+        type : String
+      }
+    }
+  }
+</script>
