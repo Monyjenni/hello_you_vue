9e085e0b207b5d210f1a6a2c139c3ef13e16e0d1
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-06-05 14:58:38 +0700

    conditional_rendering

---
+++ conditional/src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-btn v-bind="$attrs" v-on="$listeners" outlined :color="color" @click="awesome = !awesome">
+      Button
+    </v-btn>
+    <h1 v-if="awesome">
+      Mony is awesome
+    </h1>
+    <h2 v-else>Oh Fucking No</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'buttonRendering',
+  data() {
+    return {
+      awesome: false
+    }
+  },
+  props : {
+    color : {
+    type: String,
+    default : "primary" 
+    }
+  }
+}
+</script>
