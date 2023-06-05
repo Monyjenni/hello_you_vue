9b0a47692e45219d6d1f1e459d16d48c52e542c8
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-06-05 14:59:19 +0700

    conditional_rendering

---
+++ conditional/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div >
+    <buttonRendering color="primary"/>
+    <div v-if="type === 'A' ">
+      A
+    </div>
+    <div v-else-if="type==='B' " >
+      B
+    </div>
+    <div v-else-if="type==='C' " >
+      C
+    </div>
+    <div v-else-if="type==='D' " >
+      D
+    </div>
+    <div v-else>
+      Not A/B/C
+    </div>
+
+  </div>
+</template>
+<script>
+  import buttonRendering from '../components/Button'
+  export default {
+    name: 'HomePage',
+    components: {
+      buttonRendering
+    },
+  }
+</script>
